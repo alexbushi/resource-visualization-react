4dfb0985faaef635f2b176f16e79a457e14b3d63
alexbushi <alexbush@udel.edu>
2020-10-26 15:36:59 -0400

    added static temperature color calculator

--- src/utils/calculateColor.js
+++ src/utils/calculateColor.js
@@ -6,7 +6,7 @@ const calculateColor = (resource, view) => {
     let greenValue = 0;
     let blueValue = 0;
 
-    let {resourceStatus: status} = resource;
+    let {resourceStatus: status, temperature: temp} = resource;
 
     if(view === viewTypes.status) {
         if (status === 'GI') {
@@ -36,10 +36,56 @@ const calculateColor = (resource, view) => {
             blueValue = 0
         }
     }
+    else if (view === viewTypes.temperature) {
+        if (temp <= 5) {
+            redValue = temperatureColorList[0].red;
+            greenValue = temperatureColorList[0].green;
+            blueValue = temperatureColorList[0].blue;
+        }
+        else if (temp >= 5 && temp < 10) {
+            redValue = temperatureColorList[1].red;
+            greenValue = temperatureColorList[1].green;
+            blueValue = temperatureColorList[1].blue;
+        }
+        else if (temp >= 10 && temp < 15) {
+            redValue = temperatureColorList[2].red;
+            greenValue = temperatureColorList[2].green;
+            blueValue = temperatureColorList[2].blue;
+        }
+        else if (temp >= 15 && temp < 20) {
+            redValue = temperatureColorList[3].red;
+            greenValue = temperatureColorList[3].green;
+            blueValue = temperatureColorList[3].blue;
+        }
+        else if (temp >= 20 && temp < 25) {
+            redValue = temperatureColorList[4].red;
+            greenValue = temperatureColorList[4].green;
+            blueValue = temperatureColorList[4].blue;
+        }
+        else if (temp >= 25 && temp < 30) {
+            redValue = temperatureColorList[5].red;
+            greenValue = temperatureColorList[5].green;
+            blueValue = temperatureColorList[5].blue;
+        }
+        else if (temp >= 30 && temp < 35) {
+            redValue = temperatureColorList[6].red;
+            greenValue = temperatureColorList[6].green;
+            blueValue = temperatureColorList[6].blue;
+        }
+        else if (temp >= 35 && temp < 40) {
+            redValue = temperatureColorList[7].red;
+            greenValue = temperatureColorList[7].green;
+            blueValue = temperatureColorList[7].blue;
+        }
+        else {
+            redValue = temperatureColorList[8].red;
+            greenValue = temperatureColorList[8].green;
+            blueValue = temperatureColorList[8].blue;
+        }
+    }
     else {
         let {position, colorsList} = determineViewParameters(resource, view); 
 
-        // TODO: if CH then show lightning bolt symbol for both power flow views
         if (resource.resourceStatus === 'GI') {
             let positionInColorArray = (position / 100 * (colorsList.length - 1));
             let index = Math.trunc(positionInColorArray);
@@ -66,7 +112,7 @@ const determineViewParameters = (resource, view) => {
     let position = 0;
     let colorsList = [];
 
-    const {realPower, powerFlowPercent, soc, temperature} = resource;
+    const {realPower, powerFlowPercent, soc} = resource;
 
     if(view === viewTypes.powerFlowPercent) {
         position = powerFlowPercent;
@@ -75,12 +121,9 @@ const determineViewParameters = (resource, view) => {
         // Need to scale dynamically (currently -25 to 25 kW)
         position = (2 * realPower) + 50;
         colorsList = percentColorList;
-    } else if (view === viewTypes.soc) {
+    } else {
         position = soc;
         colorsList = percentColorList;
-    } else { // Implement static Temperature if/else
-        position = temperature;
-        colorsList = temperatureColorList;
     }
     
     return ({position, colorsList});
