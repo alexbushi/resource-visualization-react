6bcce6a3c9fe60c2dadf13e492d5e0749bad0491
alexbushi <alexbush@udel.edu>
2021-01-25 17:48:22 -0500

    removed pie chart for deployment

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -1,27 +1,27 @@
-import React, { Fragment } from "react";
-import { useSelector } from "react-redux";
-import ResourceSquare from "./ResourceSquare";
-import VariableLegend from "./Legend";
-import * as viewTypes from "../viewTypes";
-import CustomPieChart from "./graphs/CustomPieChart";
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import ResourceSquare from './ResourceSquare';
+import VariableLegend from './Legend';
+//import * as viewTypes from '../viewTypes';
+//import CustomPieChart from './graphs/CustomPieChart';
 
 const ResourceList = ({ view }) => {
   const resources = useSelector((state) => state.entities.resources.list);
 
-  const statusChartData = [
-    {
-      name: "Grid-Integrated",
-      value: useSelector((state) => state.entities.resources.GICount),
-    },
-    {
-      name: "Not Grid-Integrated",
-      value: useSelector((state) => state.entities.resources.NonGICount),
-    },
-  ];
+  // const statusChartData = [
+  //   {
+  //     name: 'Grid-Integrated',
+  //     value: useSelector((state) => state.entities.resources.GICount),
+  //   },
+  //   {
+  //     name: 'Not Grid-Integrated',
+  //     value: useSelector((state) => state.entities.resources.NonGICount),
+  //   },
+  // ];
 
   return (
     <Fragment>
-      <hr style={{ background: "white" }} />
+      <hr style={{ background: 'white' }} />
       <h4 className='row justify-content-start ml-2'>{view.name}</h4>
       <div className='row align-items-start mb-3'>
         <div className='col-1'>
@@ -39,12 +39,12 @@ const ResourceList = ({ view }) => {
                 />
               );
             })}
-            {view.name === viewTypes.status.name && (
+            {/* {view.name === viewTypes.status.name && (
               <CustomPieChart
                 data={statusChartData}
                 colors={["#008000", "#808080"]}
               />
-            )}
+            )} */}
           </div>
         </div>
       </div>
