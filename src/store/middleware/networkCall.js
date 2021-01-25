e42edbc5fa4c5f3ffaccf94f1ecdeb11aa0063c2
alexbushi <alexbush@udel.edu>
2021-01-25 15:43:16 -0500

    added static.json

--- src/store/middleware/networkCall.js
+++ src/store/middleware/networkCall.js
@@ -1,40 +1,54 @@
 import axios from 'axios';
 import * as actions from './networkCallActions';
-//import { baseURL } from '../../constants';
-import { determineBaseURL } from '../../constants';
+import { baseURL } from '../../constants';
+//import { determineBaseURL } from '../../constants';
 
-const api = ({ dispatch }) => next => async action => {
-    if (action.type !== actions.apiCallBegan.type) return next(action);
+const api = ({ dispatch }) => (next) => async (action) => {
+  if (action.type !== actions.apiCallBegan.type) return next(action);
 
-    const { rto, url, method, data, onStart, onSuccess, onError, stateData } = action.payload;
+  const {
+    rto,
+    url,
+    method,
+    data,
+    onStart,
+    onSuccess,
+    onError,
+    stateData,
+  } = action.payload;
 
-    if (onStart) dispatch({ type: onStart });
+  if (onStart) dispatch({ type: onStart });
 
-    next(action); 
-    
-    try {
-        const baseUrl = determineBaseURL(rto);
+  next(action);
 
-        const response = await axios.request({ 
-            //baseURL,
-            baseURL: baseUrl,
-            url,
-            method,
-            data
-        });
-        // General
-        dispatch(actions.apiCallSuccess(response.data));
-        // Specific
-        if (onSuccess && stateData) dispatch({type: onSuccess, payload: {data: response.data, stateData}})
-        else if (onSuccess) dispatch({type: onSuccess, payload: response.data, rto});
-        else {}
+  try {
+    //const baseUrl = determineBaseURL(rto);
 
-    } catch(error) {
-        // General
-        dispatch(actions.apiCallFailed(error.message));
-        // Specific
-        if (onError) dispatch({type: onError, payload: error.message});
-    } 
-}
+    const response = await axios.request({
+      baseURL,
+      //baseURL: baseUrl,
+      url,
+      method,
+      data,
+    });
+    // General
+    dispatch(actions.apiCallSuccess(response.data));
+    // Specific
+    if (onSuccess && stateData)
+      dispatch({
+        type: onSuccess,
+        payload: { data: response.data, stateData },
+      });
+    else if (onSuccess)
+      dispatch({ type: onSuccess, payload: response.data, rto });
+    else {
+    }
+  } catch (error) {
+    // General
+    dispatch(actions.apiCallFailed(error.message));
+    // Specific
+    if (onError) dispatch({ type: onError, payload: error.message });
+  }
+};
 
 export default api;
